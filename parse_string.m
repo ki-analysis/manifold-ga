ae6d0958c022577220a1d9fb5dd16a69b21ae912
rfung-uwm-edu <rfung@uwm.edu>
2019-06-18 15:24:02 -0500

    Add files via upload

---
+++ parse_string.m
@@ -0,0 +1,9 @@
+function my_substring=parse_string(my_string,marker0,marker1)
+% Copyright (c) 2019 Russell Fung & Abbas Ourmazd. All Rights Reserved.
+  
+  i0 = strfind(my_string,marker0)+length(marker0);
+  i1 = strfind(my_string,marker1)-1;
+  i1(i1<i0) = [];
+  i1 = i1(1);
+  my_substring = my_string(i0:i1);
+% end parse_string
