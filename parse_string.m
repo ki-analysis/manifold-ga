d13fabf793e30f00ceede2889ef62db7168f1c57
rfung-uwm-edu <rfung@uwm.edu>
2019-07-03 16:58:57 -0500

    dubious characters in filenames
    dubious characters inserted into filenames by Synapse removed.

--- parse_string.m
+++ parse_string.m
@@ -1,6 +1,9 @@
 function my_substring=parse_string(my_string,marker0,marker1)
 % Copyright (c) 2019 Russell Fung & Abbas Ourmazd. All Rights Reserved.
   
+  i0 = strfind(my_string,'(');
+  i1 = strfind(my_string,')');
+  my_string(i0:i1) = [];
   i0 = strfind(my_string,marker0)+length(marker0);
   i1 = strfind(my_string,marker1)-1;
   i1(i1<i0) = [];
